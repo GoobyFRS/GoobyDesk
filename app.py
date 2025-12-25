981a93a1489001b5f05eb2600f666c6ccc381ebc
Maattt GoobyFRS <68407017+GoobyFRS@users.noreply.github.com>
2025-12-24 18:32:31 -0600

    my_data push

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from dotenv import load_dotenv
 from datetime import datetime, timedelta
 from functools import wraps
 
-BUILDID=str("0.7.7-beta-f")
+BUILDID=str("0.7.7-beta-h")
 
-load_dotenv(dotenv_path=".env")
+load_dotenv(dotenv_path="my_data/.env")
 EMAIL_PASSWORD = os.getenv("EMAIL_PASSWORD") # App Password from Gmail or relevant email provider.
 CF_TURNSTILE_SITE_KEY = os.getenv("CF_TURNSTILE_SITE_KEY") # REQUIRED for CAPTCHA functionality.
 CF_TURNSTILE_SECRET_KEY = os.getenv("CF_TURNSTILE_SECRET_KEY") # REQUIRED for CAPTCHA functionality.
