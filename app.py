ce9d4a160173ae6962d360ba0ca40bb1efe7c374
Maattt GoobyFRS <68407017+GoobyFRS@users.noreply.github.com>
2026-01-07 23:10:12 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from functools import wraps
 from blueprints.api_ingest import api_ingest_bp
 from blueprints.reports_module import reports_module_bp
 
-BUILDID=str("0.9.1-beta-a")
+BUILDID=str("0.9.2-beta-a")
 
 """
 Rest in Peace Alex, July 2nd 2005 - December 14th 2024
